677392458ffcc3688a4a98e40a1a3d03bca46f55
Abhiram13 <abhiram131869@gmail.com>
2021-09-15 09:28:13 +0530

    commit

--- CustomerServices/ContentView.swift
+++ CustomerServices/ContentView.swift
@@ -10,12 +10,8 @@ import CoreData
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello Mac OS");
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        VStack {
+            Text("Hello Mac OS");
+        }.frame(minWidth: 400, idealWidth: 600, minHeight: 450, idealHeight: 800)
     }
 }
